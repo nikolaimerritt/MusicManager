51e77fe25c5ad518e457715d90d58842813c3da6
nikolaimerritt <nmerritt.62761@farnborough.ac.uk>
2017-06-22 10:22:19 +0100

    Added a very basic UI
    Added a button that will play a certain, preset MP3 file. If you press play multiple times, it will play the same file multiple times at once -- need to fix this

--- Main.java
+++ Main.java
@@ -1,16 +1,61 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
+
 
 public class Main
 {
 
+    public static MediaPlayer getMediaPlayer(String location)
+    {
+        Media media = new Media(new File(location).toURI().toString());
+        return new MediaPlayer(media);
+    }
+
+    public static void initialiseGUI()
+    {
+        // setting up stage
+        Stage stage = new Stage();
+        stage.setTitle("Music Player");
+        stage.setResizable(true);
+
+        // setting up GridPane controller
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+
+        // defining play button -- will play a preset MP3 file
+        Button playBtn = new Button("Play!");
+        playBtn.setOnAction((ActionEvent ae) -> getMediaPlayer("/Users/seraph/Downloads/TestMP3.mp3").play());
+        GridPane.setConstraints(playBtn, 0, 0);
+        grid.getChildren().add(playBtn);
+
+        // finally making stage visible
+        stage.setScene(new Scene(grid, 300, 300));
+        stage.setWidth(300);
+        stage.setHeight(300);
+        stage.show();
+    }
+
+    public static void launchFX()
+    {
+        new JFXPanel();
+        Platform.runLater(() -> initialiseGUI());
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        launchFX();
     }
 }
