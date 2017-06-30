7c487ce55a7e47e3729e7a2252253339b62eab9d
nikolaimerritt <nmerritt.62761@farnborough.ac.uk>
2017-06-30 10:02:53 +0100

    Single playlist editor now edits playlists!
    Also built general-purpose sftp writer function

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
@@ -11,6 +12,10 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.stage.WindowEvent;
+
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.lang.reflect.Array;
 import java.net.URL;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -19,12 +24,13 @@ import javazoom.jl.decoder.JavaLayerException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import com.jcraft.jsch.*;
 
 public class Main extends Application
 {
     private static QueuePlayer queuePlayer = new QueuePlayer();
     static final String rootURL = "http://musicmanager.duckdns.org/";
-    private static final HashMap<String, String[]> playlistHashMap = getPlaylistHashMap();
+    private static final HashMap<String, ArrayList<String>> playlistHashMap = getPlaylistHashMap();
     private static int viewMode = ViewMode.MUSIC_OVERVIEW;
     private static final ArrayList<String> allTracks = getFileNamesAtSite(rootURL + "AllTracks/");
     static ArrayList<String> trackQueue = allTracks;
@@ -100,22 +106,25 @@ public class Main extends Application
         mainListView.setOnMouseClicked(event ->
         {
             String selectedItem = mainListView.getSelectionModel().getSelectedItem();
-            switch(viewMode)
+            if (selectedItem != null)
             {
-                case ViewMode.MUSIC_OVERVIEW:
-                    queuePlayer.stopQueue();
-                    while (!trackQueue.get(0).equals(selectedItem))
-                    {
-                        trackQueue = shiftLeft(trackQueue);
-                    }
-                    queuePlayer.playNewQueue(0);
-                    break;
+                switch(viewMode)
+                {
+                    case ViewMode.MUSIC_OVERVIEW:
+                        queuePlayer.stopQueue();
+                        while (!trackQueue.get(0).equals(selectedItem))
+                        {
+                            trackQueue = shiftLeft(trackQueue);
+                        }
+                        queuePlayer.playNewQueue(0);
+                        break;
 
-                case ViewMode.PLAYLIST_OVERVIEW:
-                    showEditSinglePlaylistScene(selectedItem);
-                    break;
+                    case ViewMode.PLAYLIST_OVERVIEW:
+                        showEditSinglePlaylistScene(selectedItem);
+                        break;
 
-                default: break;
+                    default: break;
+                }
             }
         });
         GridPane.setConstraints(mainListView, 0, 1, 100, 1);
@@ -159,6 +168,7 @@ public class Main extends Application
                 case ViewMode.MUSIC_OVERVIEW: // should be PLAYLIST_OVERVIEW
                     viewMode = ViewMode.PLAYLIST_OVERVIEW;
                     final ArrayList<String> playlistNames = new ArrayList<>(playlistHashMap.keySet());
+                    Collections.sort(playlistNames);
                     mainListView.setItems(FXCollections.observableArrayList(playlistNames));
                     break;
 
@@ -265,10 +275,33 @@ public class Main extends Application
     {
         Stage editPlaylistStage = new Stage();
         GridPane editPlaylistGrid = new GridPane();
+        ObservableList<String> playlistTracks = FXCollections.observableArrayList(playlistHashMap.get(playlistName));
 
         editPlaylistStage.setTitle("Edit Playlist: " + playlistName);
         editPlaylistStage.setResizable(true);
-        editPlaylistStage.setOnCloseRequest((WindowEvent event) -> editPlaylistStage.hide());
+        editPlaylistStage.setOnCloseRequest(event ->
+        {
+            // saving all playlists to file, with updates. each line in format: playlistName:song1.mp3;song2.mp3;song3.mp3 ...
+            String writeInFile = "";
+            for (Object objectEntry : playlistHashMap.entrySet())
+            {
+                final Map.Entry item = (Map.Entry) objectEntry;
+                final ObservableList<String> currentPlaylistContents;
+                final String currentPlaylistName = (String) item.getKey();
+                if (item.getKey() == playlistName) { currentPlaylistContents = playlistTracks; }
+                else { currentPlaylistContents = FXCollections.observableArrayList((ArrayList<String>) item.getValue()); } // <3 <3 <3
+
+                writeInFile += currentPlaylistName + ":";
+                for (int i = 0; i < currentPlaylistContents.size(); i++)
+                {
+                    writeInFile += currentPlaylistContents.get(i);
+                    writeInFile += ((i + 1 == currentPlaylistContents.size()) ? "\n" : ";");
+                }
+                final byte[] password = new byte[]{(byte) 0x65, (byte) 0x6e, (byte) 0x75, (byte) 0x6d, (byte) 0x61, (byte) 0x45, (byte) 0x6c, (byte) 0x69, (byte) 0x5f, (byte) 0x73};
+                sftpWriter("www.musicmanager.duckdns.org", "pi", password, "/var/www/html/Playlists/playlist.txt", writeInFile);
+            }
+            editPlaylistStage.hide();
+        });
         Scene editPlaylistScene = new Scene(editPlaylistGrid);
         editPlaylistScene.getStylesheets().add(Main.class.getResource("Main.css").toExternalForm());
         editPlaylistStage.setWidth(550);
@@ -279,6 +312,51 @@ public class Main extends Application
         editPlaylistGrid.setPadding(new Insets(10, 10, 10, 10));
         editPlaylistGrid.setVgap(5);
         editPlaylistGrid.setHgap(5);
+
+        // setting up playlist header label
+        final Label playlistLabel = new Label(playlistName.toUpperCase());
+        GridPane.setConstraints(playlistLabel, 0, 0, 60, 1);
+        editPlaylistGrid.getChildren().add(playlistLabel);
+
+        // setting up all tracks header label
+        final Label allTracksLabel = new Label("ALL TRACKS");
+        GridPane.setConstraints(allTracksLabel, 62, 0, 35, 1);
+        editPlaylistGrid.getChildren().add(allTracksLabel);
+
+        // setting up search box for all tracks
+
+        // setting up playlist listview. removes track from playlist if clicked on
+        final ListView<String> playlistListView = new ListView<>(playlistTracks);
+        playlistListView.setOnMouseClicked(event -> playlistTracks.remove(playlistListView.getSelectionModel().getSelectedItem()));
+        GridPane.setConstraints(playlistListView, 0, 1, 60, 11);
+        editPlaylistGrid.getChildren().add(playlistListView);
+
+        // setting up all tracks listview. adds track to playlist if clicked on
+        final ListView<String> allTracksListView = new ListView<>(FXCollections.observableArrayList(allTracks));
+        allTracksListView.setOnMouseClicked(event ->
+        {
+            String trackToAdd = allTracksListView.getSelectionModel().getSelectedItem();
+            if (!playlistTracks.contains(trackToAdd))
+            {
+                playlistTracks.add(trackToAdd);
+            }
+        });
+        GridPane.setConstraints(allTracksListView, 62, 2, 35, 10);
+        editPlaylistGrid.getChildren().add(allTracksListView);
+
+        final TextField searchField = new TextField();
+        searchField.setPromptText("⚲");
+        searchField.textProperty().addListener((observable, oldValue, newValue) ->
+        {
+            String searchText = newValue.toUpperCase();
+            allTracksListView.getItems().clear();
+            allTracks.forEach((trackName ->
+            {
+                if (trackName.toUpperCase().contains(searchText)) { allTracksListView.getItems().add(trackName); }
+            }));
+        });
+        GridPane.setConstraints(searchField, 62, 1,35, 1);
+        editPlaylistGrid.getChildren().add(searchField);
 
         // finally showing editMusicStage
         editPlaylistStage.show();
@@ -308,11 +386,11 @@ public class Main extends Application
         return songNames;
     }
 
-    private static HashMap<String, String[]> getPlaylistHashMap()
+    private static HashMap<String, ArrayList<String>> getPlaylistHashMap()
     {
         final URL url;
         final Scanner scanner;
-        HashMap<String, String[]> playlistHashMap = new HashMap<>();
+        HashMap<String, ArrayList<String>> playlistHashMap = new HashMap<>();
         try { url = new URL("http://musicmanager.duckdns.org/Playlists/playlist.txt"); }
         catch (MalformedURLException ex) { throw new RuntimeException(ex); }
         try { scanner = new Scanner(url.openStream()); }
@@ -323,10 +401,35 @@ public class Main extends Application
             // each line is in format playlistName:foo.mp3;bar.mp3;foobar.mp3
             String[] splitOnPlaylist = scanner.nextLine().split(":");
             String playlistName = splitOnPlaylist[0]; // bit before ':'
-            String[] itemsInPlaylist = splitOnPlaylist[1].split(";"); // getting all items after playlist, as array
+            ArrayList<String> itemsInPlaylist = new ArrayList<>(Arrays.asList(splitOnPlaylist[1].split(";"))); // getting all items after playlist, as array
             playlistHashMap.put(playlistName, itemsInPlaylist);
         }
         return playlistHashMap;
+    }
+
+    private void sftpWriter(final String hostName, final String username, final byte[] password, final String destination, final String toWrite)
+    {
+        final JSch jSch = new JSch();
+        final Session session;
+        final Writer writer;
+        //final byte[] password = new byte[]{(byte) 0x65, (byte) 0x6e, (byte) 0x75, (byte) 0x6d, (byte) 0x61, (byte) 0x45, (byte) 0x6c, (byte) 0x69, (byte) 0x5f, (byte) 0x73 };
+        try
+        {
+            session = jSch.getSession(username, hostName, 22);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.setPassword(password);
+            session.connect();
+
+            Channel channel = session.openChannel("sftp");
+            channel.connect();
+            ChannelSftp channelSftp = (ChannelSftp) channel;
+            writer = new OutputStreamWriter(channelSftp.put(destination), "utf-8");
+            writer.write(toWrite);
+            writer.close();
+            channelSftp.exit();
+            session.disconnect();
+        }
+        catch (JSchException | IOException | SftpException ex) { throw new RuntimeException(ex); }
     }
 
     public static void main(String[] args) throws InterruptedException
